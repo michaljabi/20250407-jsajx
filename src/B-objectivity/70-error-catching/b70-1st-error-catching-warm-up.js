682a33fa6f814389d94cd3717f53ec9cb4a32f1c
Jabi <michal.jabi@gmail.com>
2025-04-08 16:07:35 +0200

    b70(wp): solved - errors try/catch

--- src/B-objectivity/70-error-catching/b70-1st-error-catching-warm-up.js
+++ src/B-objectivity/70-error-catching/b70-1st-error-catching-warm-up.js
@@ -10,10 +10,22 @@ import { assertThat } from '../../j4b1-assert.js'
  * - nie można zmieniać istniejącego kodu.
  */
 
+try {
+	JSON.parse('{ ; }') //=
+	console.log('I will not run...')
+} catch (e) {
+	console.log(e.message);
+} finally {
+	console.log('I will ALWAYS run')
+}
+
 let superInvention = 'What was the last one? Oh yes. ';
 
-
-somethingThatWillAlwaysBeBroken( true );
+try {
+	somethingThatWillAlwaysBeBroken(true);
+} catch (e) {
+	console.log(e.message)
+}
 
 
 superInvention += 'A ladder, to help moths escape from the bath.';
@@ -21,8 +33,8 @@ superInvention += 'A ladder, to help moths escape from the bath.';
 // #Reguła:
 // Nie możesz zmieniać kodu poniżej:
 function somethingThatWillAlwaysBeBroken(breakIt) {
-	if(breakIt) {
-		throw new Error( 'You shall not pass!' );
+	if (breakIt) {
+		throw new Error('You shall not pass!');
 	}
 }
 
