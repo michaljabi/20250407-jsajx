fa3b194e1ff74fbddfccdec1608a2c8b93c96b7c
Jabi <michal.jabi@gmail.com>
2025-04-08 12:39:25 +0200

    b50(wp): make it work.

--- src/B-objectivity/50-setters-getters/b50-1st-setters-getters-warm-up.js
+++ src/B-objectivity/50-setters-getters/b50-1st-setters-getters-warm-up.js
@@ -1,4 +1,6 @@
 import { assertThat } from '../../j4b1-assert.js'
+
+import { stringHelper } from '../../A-the-modular-js/20-import-export-variants/a20-challenge-project/czesuaf-util.js'
 /**
  * b50-setters-getters
  * Warm up
@@ -9,7 +11,22 @@ import { assertThat } from '../../j4b1-assert.js'
 const person = {
 	// #Reguła:
 	// Kodzik można pisać tylko tutaj w środku.
+	name: 'Janusz',
+	lastName: 'Kowalski',
+	get email() {
+		return `${this.name}.${this.lastName}@workload.com`.toLowerCase();
+	},
+	set email(value) {
+		const [nameLastname] = value.split('@')
+		const [name, lastName] = nameLastname.split('.');
+
+		this.name = stringHelper.capitalize(name);
+		this.lastName = stringHelper.capitalize(lastName);
+		// this.email = value // UWAGA - ukryta rekurencja ! to wywoła ten sam setter ponownie.
+	}
 }
+
+// person.email = 'marek.nowak@workload.com'
 
 // #Reguła:
 // Nie możesz zmieniać kodu poniżej:
