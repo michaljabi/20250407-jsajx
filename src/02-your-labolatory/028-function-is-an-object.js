4a647a399b2c6b71a8f47d25d6b5e9781b1aa2c7
Jabi <michal.jabi@gmail.com>
2025-04-08 10:28:51 +0200

    028(explain): prove that function is an object

---
+++ src/02-your-labolatory/028-function-is-an-object.js
@@ -0,0 +1,18 @@
+function User() {
+
+}
+
+
+User();
+
+console.log(User instanceof Function)
+console.log(User instanceof Object)
+
+
+console.log(User.name);
+console.log(User.prototype);
+
+console.log(User.bind) 
+console.log(User.call) 
+console.log(User.apply) 
+
