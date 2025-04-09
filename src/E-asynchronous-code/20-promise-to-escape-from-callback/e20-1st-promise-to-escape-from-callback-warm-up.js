65188607e5c3e653c75e691ac23bebb80229f78c
Jabi <michal.jabi@gmail.com>
2025-04-09 12:33:35 +0200

    e20(wp): solved with .then + .catch

--- src/E-asynchronous-code/20-promise-to-escape-from-callback/e20-1st-promise-to-escape-from-callback-warm-up.js
+++ src/E-asynchronous-code/20-promise-to-escape-from-callback/e20-1st-promise-to-escape-from-callback-warm-up.js
@@ -10,10 +10,14 @@ import { assertThat } from '../../j4b1-assert.js'
  */
 
 let name = '';
-Promise.resolve('Roy')
+Promise.resolve('Roy').then(value => {
+	name = value;
+})
 
 let lastName = '';
-Promise.reject('Trenneman')
+Promise.reject('Trenneman').catch(value => {
+	lastName = value;
+})
 
 // #Reguła:
 // Nie można zmieniać kodu poniżej:
